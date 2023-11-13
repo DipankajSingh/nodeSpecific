71d864f52e45f46a764bea06325f524b6f4d8696
Dipankaj Singh <dipankajsingh2@.com>
2023-11-14 01:27:52 +0530

    finally something

--- index.js
+++ index.js
@@ -1,43 +1,124 @@
-const fs = require('fs');
-const { PDFDocument } = require('pdf-lib');
+import * as pdf_Dist from 'pdfjs-dist';
+import fs from 'fs';
+import { PDFDocument, range } from "pdf-lib"
+import { homedir } from 'os';
+import keypress from 'keypress';
+import { clear } from 'console';
 
-const inputPdfPath = 'input.pdf';
-const outputPdfPath = 'output.pdf';
+const downloadDir = homedir() + '/Downloads';
 
-async function removeBlankPages(inputPath, outputPath) {
-    const pdfBuffer = fs.readFileSync(inputPath);
-    const pdfDoc = await PDFDocument.load(pdfBuffer);
+// create a new folder called no_blanks on the download directory
+try {
+    fs.mkdirSync(downloadDir + '/no_blanks');
+} catch (error) {
+    if (error.code !== 'EEXIST') {
+        console.log('\x1b[41m 0_0 nice! \x1b[0m');
+    }
+}
+const outPutFolder = homedir() + '/Downloads/no_blanks';
 
-    // Create a new PDF document to store non-blank pages
-    const newPdfDoc = await PDFDocument.create();
+fs.watch(downloadDir, { persistent: true }, (event, filename) => {
+    console.log(event)
+    //return if the event type is Unconfirmed
+    if (event !== 'rename') {
+        return
+    }
 
-    const pagePromises = pdfDoc.getPages().map(async (page, pageIndex) => {
-        // Extract page text content
-        const text = await page.getTextContent();
+    //check if the file is exists
+    if (!fs.existsSync(downloadDir + '/' + filename)) {
+        console.log('\x1b[41m ok deleted \x1b[0m');
+        return
+    }
 
-        // Check if the page has any text
-        const hasText = text.items.length > 0;
+    // check if the file is a pdf with regex
+    if (filename.match(/\.pdf$/)) {
+        console.log("New file: " + filename);
+        console.log("\x1b[51m Removing blank pages... \x1b[0m");
+        removeBlankPages(downloadDir + '/' + filename)
+    }
 
-        if (hasText) {
-            // If the page has text, add it to the new PDF
-            const copiedPage = await newPdfDoc.copyPage(page);
-            newPdfDoc.addPage(copiedPage);
+
+})
+
+
+function removeBlankPages(filePath) {
+    pdf_Dist.getDocument(filePath).promise.then(async (pdfDoc) => {
+        let pageToInclude = []
+        for (let i = 1; i <= pdfDoc.numPages; i++) {
+            await pdfDoc.getPage(i).then(async (page) => {
+
+                const text = await page.getTextContent();
+                if (text.items.length > 0) {
+                    pageToInclude.push(i - 1)
+                }
+
+            });
         }
+
+        const pdfFileBuffer = fs.readFileSync(filePath);
+
+
+        PDFDocument.load(pdfFileBuffer).then(async (PDFDoc) => {
+            const oldPages = PDFDoc.getPages()
+            const newPdfDoc = await PDFDocument.create()
+
+            pageToInclude.sort()
+
+            const copiesOfPages = await newPdfDoc.copyPages(PDFDoc, pageToInclude)
+            copiesOfPages.forEach((page) => {
+                newPdfDoc.addPage(page)
+            })
+            const newDocBuffer = await newPdfDoc.save()
+
+            fs.writeFileSync(outPutFolder + `/no_blanks-${new Date().getTime()}.pdf`, newDocBuffer)
+
+        })
+        process.stdin.pause();
+        clear()
+        console.log('finished')
+        console.log(pageToInclude.length, ': pages included')
+
     });
-
-    await Promise.all(pagePromises);
-
-    // Serialize the new PDF document to a buffer
-    const newPdfBytes = await newPdfDoc.save();
-
-    // Write the buffer to the output file
-    fs.writeFileSync(outputPath, newPdfBytes);
 }
 
-removeBlankPages(inputPdfPath, outputPdfPath)
-    .then(() => {
-        console.log('Blank pages removed successfully.');
-    })
-    .catch((error) => {
-        console.error('Error removing blank pages:', error);
-    });
+// now tell the user that the setup is complete
+console.log('\x1b[32m Setup is complete o_O \x1b[0m');
+console.log("watching for pdf files in ", downloadDir)
+
+// pdf_Dist.getDocument("ROUND-1.pdf").promise.then(async (pdfDoc) => {
+//     let pageToInclude = []
+//     for (let i = 1; i <= pdfDoc.numPages; i++) {
+//         await pdfDoc.getPage(i).then(async (page) => {
+
+//             const text = await page.getTextContent();
+//             if (text.items.length > 0) {
+//                 pageToInclude.push(i - 1)
+//             }
+
+//         });
+//     }
+
+//     const pdfFileBuffer = fs.readFileSync('./ROUND-1.pdf');
+
+
+//     PDFDocument.load(pdfFileBuffer).then(async (PDFDoc) => {
+//         const oldPages = PDFDoc.getPages()
+//         const newPdfDoc = await PDFDocument.create()
+
+//         pageToInclude.sort()
+
+//         const copiesOfPages = await newPdfDoc.copyPages(PDFDoc, pageToInclude)
+//         copiesOfPages.forEach((page) => {
+//             newPdfDoc.addPage(page)
+//         })
+//         const newDocBuffer = await newPdfDoc.save()
+
+//         fs.writeFileSync("./output" + '/no_blanks.pdf', newDocBuffer)
+
+//     })
+//     process.stdin.pause();
+//     clear()
+//     console.log('finished')
+//     console.log(pageToInclude.length, ': pages included')
+
+// });
